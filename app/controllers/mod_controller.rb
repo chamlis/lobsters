487fa4d50ef049ba4277084262c3ed213f6990c8
Olivia Reynolds <olivia@naff.dev>
2025-03-29 18:52:47 +0000

    fix 1+n query on /mod

--- app/controllers/mod_controller.rb
+++ app/controllers/mod_controller.rb
@@ -11,7 +11,7 @@ class ModController < ApplicationController
   def index
     @title = "Activity by Other Mods"
     @moderations = Moderation
-      .eager_load(:moderator, :story, :tag, :user, comment: [:story, :user])
+      .eager_load(:moderator, :story, :tag, :user, :category, comment: [:story, :user])
       .where.not(moderator_user_id: @user.id)
       .or(Moderation.where(moderator_user_id: nil))
       .where({moderations: {created_at: 1.month.ago..}})
